628db0d0bf592bd0e488a3724b89f536966e05e1
nanebo13 <d.o.kostenko@student.itc.khai.edu>
2024-12-20 01:06:32 +0200

    Zoo 2-nd stage

--- Zoo/zoo.py
+++ Zoo/zoo.py
@@ -2,3 +2,23 @@ print("I Love animals!")
 print("Let's check out the animals...")
 print("The deer lokks fine.")
 print("The lion looks healthy")
+camel = r"""
+ ___.-''''-.
+/___  @    |
+',,,,.     |         _.'''''''._
+     '     |        /           \
+     |     \    _.-'             \
+     |      '.-'                  '-.
+     |                               ',
+     |                                '',
+      ',,-,                           ':;
+           ',,| ;,,                 ,' ;;
+              ! ; !'',,,',',,,,'!  ;   ;:
+             : ;  ! !       ! ! ;  ;   :;
+             ; ;   ! !      ! !  ; ;   ;,
+            ; ;    ! !     ! !   ; ;
+            ; ;    ! !    ! !     ; ;
+           ;,,      !,!   !,!     ;,;
+           /_I      L_I   L_I     /_I
+"""
+print(camel)
